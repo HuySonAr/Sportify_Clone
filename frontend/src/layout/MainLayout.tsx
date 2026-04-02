25720f8851bf1207a8f9f8ef2ac954fe583c3262
HuySonAr <huysonjhs2004@gmail.com>
2026-04-02 23:18:43 +0700

    update nav listen footer

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -8,6 +8,7 @@ import { Outlet } from 'react-router-dom';
 import LeftSidebar from './components/LeftSidebar';
 import FriendsActivity from './components/FriendsActivity';
 import AudioPlayer from './components/AudioPlayer';
+import PlaybackControls from './components/PlaybackControls';
 
 const MainLayout = () => {
   const isMobile = false;
@@ -47,6 +48,8 @@ const MainLayout = () => {
           <FriendsActivity />
         </ResizablePanel>
       </ResizablePanelGroup>
+
+      <PlaybackControls />
     </div>
   );
 };
